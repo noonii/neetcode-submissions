e1c052daf247efe58617bfd1d6a1560de643708a
noonii <ssatiq.r@gmail.com>
2026-05-26 22:56:29 -0700

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights: number[]): number {
+        let l = 0;
+        let r = heights.length - 1;
+        let max = 0;
+        
+        while (l < r) {
+            const area = (r - l) * Math.min(heights[l], heights[r]);
+            max = Math.max(area, max);
+            if (heights[l] < heights[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return max;
+    }
+}
