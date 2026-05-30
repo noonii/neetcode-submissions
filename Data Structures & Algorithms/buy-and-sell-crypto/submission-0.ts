aa26bc231518315ceccc8244d0ec56659ecd483c
noonii <ssatiq.r@gmail.com>
2026-05-30 12:17:13 -0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let minBuy = prices[0];
+        let maxProfit = 0;
+
+        for (let i = 1; i < prices.length; i++) {
+            minBuy = Math.min(minBuy, prices[i]);
+
+            const profit = prices[i] - minBuy;
+
+            maxProfit = Math.max(maxProfit, profit);
+        }
+
+        return maxProfit;
+    }
+}
