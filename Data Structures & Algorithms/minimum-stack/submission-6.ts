df4e063854a11891dd6969734edd39dbc469f3a8
noonii <ssatiq.r@gmail.com>
2026-06-03 21:55:19 -0700

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.ts
@@ -0,0 +1,42 @@
+class MinStack {
+    private stack: number[];
+    private minStack: number[];
+
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this.stack.push(val);
+
+        const min = this.getMin();
+        this.minStack.push(isNaN(min) ? val : Math.min(min, val));
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
