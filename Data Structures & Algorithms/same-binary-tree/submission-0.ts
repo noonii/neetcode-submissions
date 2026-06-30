de5d892d11969078a4aab8e6df64a8d3cb2c36a5
noonii <ssatiq.r@gmail.com>
2026-06-29 21:45:18 -0700

    Add: same-binary-tree - submission-0

---
+++ Data Structures & Algorithms/same-binary-tree/submission-0.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+        if (!q && !p) return true;
+
+        if (!this.isNodeEqual(p, q)) {
+            return false;
+        }
+
+        return this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right);
+    }
+
+    isNodeEqual(a: TreeNode, b: TreeNode): boolean {
+        return a && b && a.val === b.val;
+    }
+}
