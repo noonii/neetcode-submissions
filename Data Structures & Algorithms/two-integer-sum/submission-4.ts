990df3128be87db2818017e7d63e63d02230d2a6
noonii <ssatiq.r@gmail.com>
2026-07-08 17:18:40 -0700

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        if (!nums || !nums.length || isNaN(target)) return null;
+        if (nums.length < 2) return null;
+
+        const seen = new Map<number, number>();
+
+        for (let x = 0; x < nums.length; x++) {
+            const diff = target - nums[x];
+            if (seen.has(diff)) {
+                return [x, seen.get(diff)];
+            }
+            seen.set(nums[x], x);
+        }
+
+        return [];
+    }
+}
