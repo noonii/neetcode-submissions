13115e4b825115f4004511ecce898e7df4cf102a
noonii <ssatiq.r@gmail.com>
2026-07-09 21:40:56 -0700

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.ts
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        return strs.map((str) => `${str.length}#${str}`).join("");
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        // 5#Hello5#World
+        const res: string[] = [];
+        let x = 0;
+        while (x < str.length) {
+            let len = 0;
+            while (true) {
+                const c = str.charCodeAt(x);
+                if (c === 35) break;
+                len = len * 10 + c - 48;
+                x++;
+            }
+            const beg = x+1;
+            const end = beg + len;
+            res.push(str.substring(beg, end));
+            x = end
+        }
+
+        return res;
+    }
+}
